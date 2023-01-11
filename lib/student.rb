413ce79c99e14f07e443fe47d4be4b3b49852fc6
Sean Welsh <s.welsh1314@gmail.com>
2023-01-11 13:42:07 -0500

    commit tree 667e4bb399ea62fdc92f400f908f16fc69bef1ac

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative './user.rb'
+require 'pry'
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize(knowledge =[])
+        self.knowledge = knowledge
+       
+    end
+
+    def learn(string)
+        self.knowledge << string
+        
+    end
 
 end
+
+me = Student.new
---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+    attr_accessor :first_name, :last_name
+
+    # def initialize(first, last)
+    #     self.first = first
+    #     self.last = last
+    # end
+
+end
